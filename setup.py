c6e21faab04a4ba4d54812905ec8ebc1a65a65e7
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2021-04-21 16:11:48 +0100

    Use setup.cfg

--- setup.py
+++ setup.py
@@ -1,51 +1,5 @@
 from setuptools import setup
 
-with open("README.rst") as f:
-    long_description = f.read()
-
 setup(
-    name="tszip",
-    description="Compression utilities for tree sequences",
-    long_description=long_description,
-    packages=["tszip"],
-    author="Tskit Developers",
-    author_email="jerome.kelleher@well.ox.ac.uk",
-    url="http://pypi.python.org/pypi/tszip",
-    setup_requires=["setuptools_scm"],
-    install_requires=[
-        "numpy",
-        "humanize",
-        "tskit",
-        "numcodecs",
-        "zarr",
-    ],
-    keywords=["Tree sequences", "tskit"],
-    license="MIT",
-    platforms=["POSIX", "Windows", "MacOS X"],
-    python_requires=">=3.7",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3 :: Only",
-        "License :: OSI Approved :: MIT License",
-        "Development Status :: 3 - Alpha",
-        "Environment :: Other Environment",
-        "Intended Audience :: Science/Research",
-        "Operating System :: POSIX",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Bio-Informatics",
-    ],
-    project_urls={
-        "Bug Reports": "https://github.com/tskit-dev/tszip/issues",
-        "Source": "https://github.com/tskit-dev/tszip",
-    },
-    entry_points={
-        "console_scripts": [
-            "tszip=tszip.cli:tszip_main",
-            "tsunzip=tszip.cli:tsunzip_main",
-        ]
-    },
     use_scm_version={"write_to": "tszip/_version.py"},
 )
